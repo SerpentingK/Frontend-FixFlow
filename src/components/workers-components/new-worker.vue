101cdcd57df8ba3fa909a7e283b4136d1fa4c15c
David Carrillo <david.carrillo.moncayo@gmail.com>
2024-11-06 00:09:42 -0500

    Update new-worker.vue

--- src/components/workers-components/new-worker.vue
+++ src/components/workers-components/new-worker.vue
@@ -1,3 +1,17 @@
 <template>
-    b
+    <section class="form-container">
+        <h2>Crear Colaborador</h2>
+        <label class="input-container">
+            <span></span>
+            <input type="text" name="" placeholder="Nombre">
+        </label>
+        <label class="input-container" >
+            <span></span>
+            <input type="text" name="" placeholder="Documento">
+        </label>
+        <label class="input-container">
+            <span></span>
+            <input type="text" name="" placeholder="Clave">
+        </label>
+    </section>
 </template>
