48e682eff719b01ac289daa02e32004c85e5cd1c
David Carrillo <david.carrillo.moncayo@gmail.com>
2025-04-04 15:34:39 -0500

    a

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import "./assets/base.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./routers/routes";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
 
 const app = createApp(App);
 
 app.use(router);
-app.mount("#app");
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
